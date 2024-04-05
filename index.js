8fc08de2c1f58675ee4f6a46d862c9e9d874ccb0
Callw02 <muficcalle@gmail.com>
2024-04-05 11:40:54 +0200

    push

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 let goToLayout = (layoutName) => {
   window.location.href = `?layout=${layoutName}`;
 };
@@ -11,13 +13,19 @@ if (localStorage.getItem("username")) {
 } else {
   signUppage();
 }
+let layoutUrl = window.location.search.split("?layout=")[1];
+let numKeys = window.localStorage.getItem("keysFound");
+let numLayout = parseInt(layoutUrl.match(/\d+/));
 
-if (window.location.search.includes("layout")) {
-  let layoutUrl = window.location.search.split("?layout=")[1];
-  let numKeys = window.localStorage.getItem("keysFound");
-  let numLayout = parseInt(layoutUrl.match(/\d+/));
+if (numLayout >= parseInt(numKeys)) {
+    RenderStartingpage();
+    history.pushState(null, "", "?layout=layout0");
+  }
 
-  if (parseInt(numKeys) === numLayout) {
+function checkLayout(){
+  if (window.location.search.includes("layout")) {
+
+  if (parseInt(numKeys) >= numLayout) {
     document.querySelector("body").innerHTML = `
         <h1>Welcome to layout${numKeys}</h1>
         <button onclick="continueToNextLayout()">Continue</button>
@@ -25,17 +33,20 @@ if (window.location.search.includes("layout")) {
   } else {
     RenderStartingpage();
     history.pushState(null, "", "?layout=layout0");
+    
   }
+}
 }
 
 function RenderStartingpage() {
+  history.pushState(null, "", "?layout=layout0");
   let username = localStorage.getItem("username");
 
   let keysFound = parseInt(window.localStorage.getItem("keysFound")) || 0;
 
   let buttonsHTML = "";
   for (let i = 1; i <= keysFound + 1; i++) {
-    buttonsHTML += `<button onclick="goToLayout('layout${i}')">Layout ${i}</button>`;
+    buttonsHTML += `<button onclick="goToLayout('layout${i}')" id="${i}">Layout ${i}</button>`;
   }
 
   document.querySelector("body").innerHTML = `
@@ -53,17 +64,26 @@ function continueToNextLayout() {
   window.localStorage.setItem("keysFound", keysFound);
 
   window.location.href = `?layout=layout${keysFound}`;
+  checkLayout()
+  
 }
 
 function displayLayoutName() {
   const urlParams = new URLSearchParams(window.location.search);
   const layoutNumber = urlParams.get("layout").replace("layout", "");
+  console.log(layoutNumber);
 
   document.body.innerHTML = "";
+if(layoutNumber === "0"){
+  RenderStartingpage()
+}else{
 
-  const h1Element = document.createElement("h1");
-
-  h1Element.textContent = `Layout ${layoutNumber}`;
-
-  document.body.appendChild(h1Element);
+  document.querySelector("body").innerHTML = `
+  <h1>Welcome to Layout ${layoutNumber}</h1>
+  <button id="goHome"> Go Home</button>
+  `
+  document.getElementById("goHome").addEventListener("click", event => {
+    RenderStartingpage()
+  })
+}
 }
