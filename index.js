673a1246bc89008fab3170c8b4a93e93dc8f64e3
Callw02 <muficcalle@gmail.com>
2024-04-05 14:20:16 +0200

    fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
-
 let goToLayout = (layoutName) => {
+  console.log(layoutName);
   window.location.href = `?layout=${layoutName}`;
 };
 
@@ -13,30 +13,31 @@ if (localStorage.getItem("username")) {
 } else {
   signUppage();
 }
-let layoutUrl = window.location.search.split("?layout=")[1];
-let numKeys = window.localStorage.getItem("keysFound");
-let numLayout = parseInt(layoutUrl.match(/\d+/));
 
-if (numLayout >= parseInt(numKeys)) {
+
+
+function checkLayout(num) {
+  const layoutUrl = window.location.search.split("?layout=")[1];
+  const numKeys = parseInt(window.localStorage.getItem("keysFound"));
+  const numLayout = parseInt(layoutUrl.match(/\d+/));
+
+  if (num > numKeys) {
+    // If the layout number is higher than the number of keys found, redirect to starting page
     RenderStartingpage();
-    history.pushState(null, "", "?layout=layout0");
+    return;
   }
 
-function checkLayout(){
-  if (window.location.search.includes("layout")) {
-
-  if (parseInt(numKeys) >= numLayout) {
-    document.querySelector("body").innerHTML = `
-        <h1>Welcome to layout${numKeys}</h1>
-        <button onclick="continueToNextLayout()">Continue</button>
-      `;
-  } else {
+  history.pushState(null, "", `?layout=layout${num}`);
+  document.querySelector("body").innerHTML = `
+      <h1>Welcome to layout${num}</h1>
+      <button id="goHome"> Go Home</button>
+  `;
+  
+  document.getElementById("goHome").addEventListener("click", event => {
     RenderStartingpage();
-    history.pushState(null, "", "?layout=layout0");
-    
-  }
+  });
 }
-}
+
 
 function RenderStartingpage() {
   history.pushState(null, "", "?layout=layout0");
@@ -46,7 +47,7 @@ function RenderStartingpage() {
 
   let buttonsHTML = "";
   for (let i = 1; i <= keysFound + 1; i++) {
-    buttonsHTML += `<button onclick="goToLayout('layout${i}')" id="${i}">Layout ${i}</button>`;
+    buttonsHTML += `<button onclick="checkLayout(${i})" id="${i}">Layout ${i}</button>`;
   }
 
   document.querySelector("body").innerHTML = `
