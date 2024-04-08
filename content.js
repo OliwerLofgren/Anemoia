0655406954dcd0c756fd191a343d3f676be86319
Oliwer <oliwerl@outlook.com>
2024-04-08 10:20:51 +0200

    content

---
+++ content.js
@@ -0,0 +1,15 @@
+const content = {
+  layout1:
+    "USER, idag vet du att jag är borta och du vill nog ta reda på vart jag är någonstans. Fortsätt hitta ledtrådar för att veta vem jag är och vart din lärare är någonstans >:)",
+  layout2:
+    "USER, HEJ!!! Trodde aldrig att du skulle fortsätta leta efter mig, meningen var att du skulle bli rädd eller uttråkad, men men. . . ",
+  layout3:
+    "Du kommer aldrig få veta vad som har hänt din lärare. Har han gömt sig för sig misslyckade arbete eller är han död?!",
+  layout4:
+    "Det var jag, Johan som har dödat din lärare, men vart jag befinner mig kommer du aldrig ta reda på. Det finns inga bevis att det är jag som har dödat honom, så lycka till HAHAHAHA",
+  layout5:
+    "Att du inte har gett upp ännu USER. Det ska jag beundra dig för. . . Men det är de enda jag kommer beundra dig för så få inga förhoppningar. . .",
+  layout6: "USER. . . USER. . . USER. . .",
+  layout7:
+    "Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum,",
+};
