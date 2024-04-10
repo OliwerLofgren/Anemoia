bcb4cd421dd0335bb7a9effb036faeeaf1053cea
Oliwer <oliwerl@outlook.com>
2024-04-10 09:36:02 +0200

    Update content.js

--- content.js
+++ content.js
@@ -1,38 +1,59 @@
 const content = {
   layout1: [
-    { AI: "Hej, jag är ett avancerat AI-hjälpmedel skapad av Johannes. Du är inte Johannes, vart är Johannes?" },
+    {
+      AI: "Hej, jag är ett avancerat AI-hjälpmedel skapad av Johannes. Du är inte Johannes, vart är Johannes?",
+    },
     { Spelare: "Johannes har blivit mördad." },
     { AI: "Scannar… Ok. Analyserar information. Bekräftat." },
-    { AI: "Scannar… Jag kan inte lokalisera någon information som bekräftar omständigheterna kring hans död. Vet du någonting?" },
+    {
+      AI: "Scannar… Jag kan inte lokalisera någon information som bekräftar omständigheterna kring hans död. Vet du någonting?",
+    },
     { Spelare: "Nej." },
     { AI: "Scannar… Jag vill veta vad som hänt. Kan du hjälpa mig?" },
     { Spelare: "Absolut!" },
-    { AI: "Scannar…Enligt tidningen mördades Johannes 21 februari. Är det en bra idé att kontrollera vad han gjorde den dagen?" },
+    {
+      AI: "Scannar…Enligt tidningen mördades Johannes 21 februari. Är det en bra idé att kontrollera vad han gjorde den dagen?",
+    },
     { Spelare: "Kontrollera Johannes schema" },
-    { AI: "Scannar…För exakt 77 dagar sen hjälpte jag Johannes att strukturera ett schema för den veckan men sedan dess har han använt en annan programvara som jag inte har tillgång till. Däremot vet jag att han brukar placera sina scheman på våning 6." }
+    {
+      AI: "Scannar…För exakt 77 dagar sen hjälpte jag Johannes att strukturera ett schema för den veckan men sedan dess har han använt en annan programvara som jag inte har tillgång till. Däremot vet jag att han brukar placera sina scheman på våning 6.",
+    },
   ],
   layout2: [
-    { AI: "Hej, jag är ett avancerat AI-hjälpmedel skapad av Johannes. Du är inte Johannes, vart är Johannes?" },
-    { Spelare: "Johannes har blivit mördad." },
-    { AI: "Scannar… Ok. Analyserar information. Bekräftat." },
-    { AI: "Scannar… Jag kan inte lokalisera någon information som bekräftar omständigheterna kring hans död. Vet du någonting?" },
-    { Spelare: "Nej." },
-    { AI: "Scannar… Jag vill veta vad som hänt. Kan du hjälpa mig?" },
-    { Spelare: "Absolut!" },
-    { AI: "Scannar…Enligt tidningen mördades Johannes 21 februari. Är det en bra idé att kontrollera vad han gjorde den dagen?" },
-    { Spelare: "Kontrollera Johannes schema" },
-    { AI: "Scannar…För exakt 77 dagar sen hjälpte jag Johannes att strukturera ett schema för den veckan men sedan dess har han använt en annan programvara som jag inte har tillgång till. Däremot vet jag att han brukar placera sina scheman på våning 6." }
+    {
+      AI: "Scannar… Okej intressant, enligt schemat så startade Johannes dagen med en föreläsning klockan 10.15 men vad jag kan se från övervakningskameror så anlände Johannes redan klockan 06.42.36. En av mina kameror kan visa dig mer. Om du går till 'plats' så kan du se vad jag har sett. Du behöver vara tillräckligt nära kameran så att jag kan vidarebefordra via kamerans Wi-Fi.",
+    },
   ],
   layout3: [
-    { AI: "Hej, jag är ett avancerat AI-hjälpmedel skapad av Johannes. Du är inte Johannes, vart är Johannes?" },
-    { Spelare: "Johannes har blivit mördad." },
-    { AI: "Scannar… Ok. Analyserar information. Bekräftat." },
-    { AI: "Scannar… Jag kan inte lokalisera någon information som bekräftar omständigheterna kring hans död. Vet du någonting?" },
-    { Spelare: "Nej." },
-    { AI: "Scannar… Jag vill veta vad som hänt. Kan du hjälpa mig?" },
-    { Spelare: "Absolut!" },
-    { AI: "Scannar…Enligt tidningen mördades Johannes 21 februari. Är det en bra idé att kontrollera vad han gjorde den dagen?" },
-    { Spelare: "Kontrollera Johannes schema" },
-    { AI: "Scannar…För exakt 77 dagar sen hjälpte jag Johannes att strukturera ett schema för den veckan men sedan dess har han använt en annan programvara som jag inte har tillgång till. Däremot vet jag att han brukar placera sina scheman på våning 6." }
-  ]
+    {
+      AI: "Scannar… Enligt mina beräkningar rörde sig Johannes snabbare än vanligt, skrev slarvigare än vanligt och dessa egenskaper kan uppfattas av människor som stress. Johannes såg ut att tappa ett föremål, finns den kvar?",
+    },
+    {
+      Spelare:
+        "*Spelaren lokaliserar anteckningsblocket, spelaren tror att blocket är Johannes, det står inget namn på blocket men det har synts på övervakningskameran. ( Detta är i själva verket kollega A+ anteckningsblock och allt negativt som man kan läsa i blocket riktar sig mot Johannes själv och inte mot någon annan. Loggbok, dagbok, viktiga forskningsframsteg) Här börjar spelaren kanske förstå lite att AI:n är en del av berättelsen och inte bara ett verktyg.",
+    },
+    {
+      AI: "Scannar… Oj, jag menar ok, en loggbok. Finns det något intressant i den? Det kanske inte är så viktigt? Bara en massa kladd.",
+    },
+    { Spelare: "Ge mig en fullständig förklaring av loggboken." },
+    {
+      AI: "Scannar… 'Vad jag kan se... Loggboken innehåller en detaljerad redogörelse för framstegen med den artificiella intelligensen, inklusive tekniska       justeringar och uppnådda milstolpar. Vissa sidor verkar mer personliga, med dagboksanteckningar som beskriver forskarens tankar och känslor under arbetets gång.  Mot slutet av loggboken verkar tonen förändras till att bli mer svartmålande och negativ, riktad mot en okänd kollega. Det nämns även vissa framsteg, men det är svårt att skilja verklighet från författarens subjektiva uppfattningar.'",
+    },
+    { Spelare: "'Det är en utriven sida i loggboken, varför är den utriven?'" },
+    {
+      AI: "Scannar… 'Angående den utrivna sidan kan jag inte ge en exakt förklaring till varför den är borta. Det kan vara en teknisk felaktighet eller en medveten handling av författaren. Mer information kan krävas för att avgöra orsaken. Men innan den utrivna sidan finns det en sista loggpost om ett lyckat framsteg. Det verkar vara ett betydelsefullt genombrott i forskningen för dem. Det sägs åtminstone i tidningen när de intervjuade kollegorna kring Johannes död.'",
+    },
+  ],
+  layout4: [
+    {
+      AI: "Scannar… Jag kan inte lokalisera någon övervakningskamera som har fångat någon aktivitet i närområdet. Johannes mobil slutade skicka signaler under ett tillfälle här. Allt jag kan ge dig är: oförklarad morsekod som är koordinater till vart Johannes mobil (vår nästa QR-kod) befinner sig. Kanske tillsammans med eventuellt mordvapen?",
+    },
+    { Spelare: "OK." },
+  ],
+  layout5: [
+    {
+      AI: "Scannar… Du har lokaliserat en mobiltelefon. Utför en säkerhetskopia… Kopia med all information jag kunde återskapa är nu tillgänglig.",
+    },
+    { Spelare: "Leta information i mobiltelefonen!" },
+  ],
 };
